5d282542284dafaf07c7590370ee8974a0ec2ab4
AlfonsoLR <alr00048@red.ujaen.es>
2021-02-01 19:42:45 +0100

    Hello world

---
+++ RayTracing/Source/PrecompiledHeaders/stdafx.h
@@ -0,0 +1,46 @@
+// [Libraries]
+
+#include "GL/glew.h"								// Don't swap order between GL and GLFW includes!
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/constants.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+// [Interface]
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "misc/cpp/imgui_stdlib.h"
+
+// [Image]
+
+#include "lodepng.h"
+
+// [Standard libraries: basic]
+
+#include <algorithm>
+#include <cmath>
+#include <chrono>
+#include <cstdint>
+#include <execution>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+#include <thread>
+
+// [Standard libraries: data structures]
+
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
